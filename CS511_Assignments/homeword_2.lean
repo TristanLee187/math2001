9a5b0688fd06f0e920d0c14cc144606604b8d6cc
Tristan Lee <tristanlee73@gmail.com>
2024-09-18 19:11:58 -0400

    finish hw2

--- CS511_Assignments/homeword_2.lean
+++ CS511_Assignments/homeword_2.lean
@@ -48,18 +48,60 @@ example {x : ℚ} (h1 : x ^ 2 = 4) (h2 : 1 < x) : x = 2 := by
   cancel (x+2) at h3
 
 -- Exercise 2.1.9 (3)
-example (x y : ℚ) (h : x * y = 1) (h2 : x ≥ 1) : y ≤ 1 := by sorry
+example (x y : ℚ) (h : x * y = 1) (h2 : x ≥ 1) : y ≤ 1 := by
+  have h3 :=
+    calc
+      x*y = 1 := by rw[h]
+      _ > 0 := by numbers
+  cancel x at h3
+  let h4 : 0 ≤ x-1 := by addarith[h2]
+  calc
+    y ≤ y + y*(0) := by extra
+    _ ≤ y + y*(x-1) := by rel[h4]
+    _ = y+x*y-y := by ring
+    _ = y+1-y := by rw[h]
+    _ = 1 := by ring
+
+lemma ne_of_gt_Z {a b : ℤ} (h : a > b) : a ≠ b := by sorry
 
 --Exercise 2.2.4 (1)
-example {m : ℤ} (hm : m + 1 = 5) : 3 * m ≠ 6 := by sorry
+example {m : ℤ} (hm : m + 1 = 5) : 3 * m ≠ 6 := by
+  apply ne_of_gt_Z
+  calc
+    3*m = 3*(m+1)-3 := by ring
+    _ = 3*5-3 := by rw[hm]
+    _ > 6 := by numbers
 
 /- # Problem 2 -/
 
 -- Example 2.1.8
-example (a b : ℝ) (h : a ≤ b) : a ^ 3 ≤ b ^ 3 := by sorry
+example (a b : ℝ) (h : a ≤ b) : a ^ 3 ≤ b ^ 3 := by
+  let h1 : 0 ≤ b-a := by addarith[h]
+  calc
+    a^3 ≤ a^3 + (b-a)*((b-a)^2 + 3*(b+a)^2)/4 := by extra
+    _ = b^3 := by ring
 
 -- Exercise 2.1.9 (2)
-example {n : ℤ} (hn : n ^ 2 + 4 = 4 * n) : n = 2 := by sorry
+example {n : ℤ} (hn : n ^ 2 + 4 = 4 * n) : n = 2 := by
+  let h1 : n^2-4*n+4=0 := by addarith[hn]
+  let h2 :=
+    calc
+      (n-2)^2=n^2-4*n+4 := by ring
+      _ = 0 := by rw[h1]
+  let h3 : n-2=0 := by cancel 2 at h2
+  calc
+    n=2 := by addarith[h3]
+
+lemma le_antisymm_Q {a b : ℚ} (h1 : a ≤ b) (h2 : b ≤ a) : a = b := by sorry
 
 -- Exercise 2.2.4 (2)
-example {s : ℚ} (h1 : 3 * s ≤ -6) (h2 : 2 * s ≥ -4) : s = -2 := by sorry
+example {s : ℚ} (h1 : 3 * s ≤ -6) (h2 : 2 * s ≥ -4) : s = -2 := by
+  apply le_antisymm_Q
+  calc
+    s = (3*s)/3 := by ring
+    _ ≤ -6/3 := by rel[h1]
+    _ = -2 := by numbers
+  calc
+    s = (2*s)/2 := by ring
+    _ ≥ -4/2 := by rel[h2]
+    _ = -2 := by numbers
